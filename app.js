d874ded2d16757c02a0836c619e7f301186dbe9f
Alon Kigler <kiglerr@gmail.com>
2023-01-30 15:20:25 +0200

    lo

---
+++ app.js
@@ -0,0 +1,39 @@
+require("./authentication/local.authentication");
+require("./authentication/jwt.authentication");
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
+const createApplication = require("express");
+const { json } = require("express");
+
+const app = createApplication();
+
+app.use(json());
+app.use(passport.initialize());
+// app.use(createApplication.urlencoded());
+
+app.post(
+  "/login",
+  passport.authenticate("local", { session: false }),
+  async function (req, res) {
+    console.log(req.user);
+    const token = await jwt.sign(
+      {
+        hello: "world",
+        userId: req.user.id,
+        blanla: "sdsd",
+      },
+      "secret string"
+    );
+    res.send("jwt token: " + token);
+  }
+);
+
+app.use("/api", passport.authenticate("jwt", { session: false }));
+
+app.get("/api/users", function (req, res) {
+  res.send("users");
+});
+
+app.listen(4000, function () {
+  console.log("we are now listening");
+});
